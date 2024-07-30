1dee86064a332aa6626c1af1635df8ef588dff39
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-07-30 12:29:49 -0300

    edit

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { CartContext } from "../context/CartContext";
+import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import {
   collection,
@@ -55,192 +56,57 @@ function Checkout() {
     setPaymentMethod(event.target.value);
   };
 
-  const comprar = async (data) => {
-    const pedido = {
-      cliente: { ...data, paymethod: paymentMethod },
-      productos: cart,
-      total: totalPrice(),
-      fecha: Timestamp.now(),
-    };
-
-    const pedidosRef = collection(db, "pedidos");
-
-    try {
-      const docRef = await addDoc(pedidosRef, pedido);
-      setDocId(docRef.id);
-
-      for (let prod of cart) {
-        const productRef = doc(db, "products", prod.id);
-        const newStock = prod.stock - prod.quantity;
-        await updateDoc(productRef, {
-          stock: newStock,
-        });
-      }
-
-      deleteCartCheckout();
-    } catch (error) {
-      console.error("Error al realizar la compra: ", error);
-    }
-  };
-
-  // if (docId) {
-  //   return (
-  //     <>
-  //       <h1 className="text-center mt-5 mb-3">Muchas gracias por tu compra</h1>
-  //       <p className="text-center textcheckout">
-  //         Para hacer el seguimiento de tu pedido, el identificador es este:{" "}
-  //         <strong>{docId}</strong>
-  //       </p>
-  //     </>
-  //   );
-  // }
-
   return (
     <div className="container mt-5">
-      {/* <form onSubmit={handleSubmit(comprar)}> */}
       <div className="row">
-        {/* <div className="col-lg-6">
-            <h2>Información de Contacto</h2>
-            <div className="mb-3">
-              <label htmlFor="email">Dirección de correo electrónico</label>
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                name="email"
-                placeholder="Ingresa tu correo electrónico"
-                {...register("email", { required: "Email es requerido" })}
-              />
-            </div>
-            <h2>Información de Envío</h2>
-            <div className="mb-3">
-              <label htmlFor="firstName">Nombre</label>
-              <input
-                type="text"
-                className="form-control"
-                id="firstName"
-                name="firstName"
-                placeholder="Ingresa tu nombre"
-                {...register("name", { required: "Nombre es requerido" })}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="lastName">Apellido</label>
-              <input
-                type="text"
-                className="form-control"
-                id="lastName"
-                name="lastName"
-                placeholder="Ingresa tu apellido"
-                {...register("lastname", { required: "Apellido es requerido" })}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="address">Dirección</label>
-              <input
-                type="text"
-                className="form-control"
-                id="address"
-                name="address"
-                placeholder="Ingresa tu dirección"
-                {...register("address", { required: "Dirección es requerido" })}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="city">Ciudad</label>
-              <input
-                type="text"
-                className="form-control"
-                id="city"
-                name="city"
-                placeholder="Ingresa tu ciudad"
-                {...register("city", { required: "Ciudad es requerido" })}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="country">País</label>
-              <input
-                type="text"
-                className="form-control"
-                id="country"
-                name="country"
-                placeholder="Ingresa tu país"
-                {...register("country", { required: "Pais es requerido" })}
-              />
-            </div>
+        <div className="col mt-5">
+          {cart.length > 0 ? (
+            <div className="row mt-3">
+              <div className="col">
+                <h2 className="mb-3">Resumen del Pedido</h2>
+                <dl className="row">
+                  {cart.map((prod, index) => (
+                    <dl className="row" key={index}>
+                      <dt className="col-sm-2">
+                        <img className="imgCheckout" src={prod.image} alt="" />
+                      </dt>
+                      <dt className="col-sm-4">
+                        {prod.name} x {prod.quantity}
+                      </dt>
+                      <dd className="col-sm-6">
+                        {prod.price * prod.quantity}$
+                      </dd>
+                    </dl>
+                  ))}
+                </dl>
 
-            <div className="mb-3">
-              <label htmlFor="phone">Teléfono</label>
-              <input
-                type="text"
-                className="form-control"
-                id="phone"
-                name="phone"
-                placeholder="Ingresa tu número de teléfono"
-                {...register("phone", { required: "Teléfono es requerido" })}
-              />
-            </div>
-          </div> */}
-        <div className="col-lg-6">
-          {/* <div className="row">
-            <div className="col">
-              <h2>Pago</h2>
-              <div className="mb-3">
-                <label htmlFor="paymentMethod">Método de Pago</label>
-                <select
-                  className="form-select"
-                  id="paymentMethod"
-                  value={paymentMethod}
-                  onChange={handlePaymentMethodChange}
-                  required
-                >
-                  <option value="">Selecciona Método de Pago</option>
-                  <option value="CreditCard">Tarjeta de Crédito</option>
-                  <option value="PayPal">PayPal</option>
-                  <option value="MercadoPago">Mercado Pago</option>
-                </select>
+                <dl className="row">
+                  <dd className="col-sm-6">
+                    <span className="checkoutTotalPrice">{totalPrice()}$</span>
+                  </dd>
+                </dl>
+
+                <button className="btnAddCart mb-5" onClick={handleBuy}>
+                  Finalizar compra
+                </button>
+                {preferenceId && (
+                  <Wallet
+                    initialization={{ preferenceId: preferenceId }}
+                    customization={{ texts: { valueProp: "smart_option" } }}
+                  />
+                )}
               </div>
             </div>
-          </div> */}
-          <div className="row mt-3">
-            <div className="col">
-              <h2 className="mb-3">Resumen del Pedido</h2>
-              <dl className="row">
-                {cart.map((prod, index) => (
-                  <dl className="row" key={index}>
-                    <dt className="col-sm-2">
-                      <img className="imgCheckout" src={prod.image} alt="" />
-                    </dt>
-                    <dt className="col-sm-4">
-                      {prod.name} x {prod.quantity}
-                    </dt>
-                    <dd className="col-sm-6">{prod.price * prod.quantity}$</dd>
-                  </dl>
-                ))}
-              </dl>
-
-              <dl className="row">
-                <dd className="col-sm-6">
-                  <span className="checkoutTotalPrice">{totalPrice()}$</span>
-                </dd>
-              </dl>
-              {/* {<button className="btnAddCart" type="submit">
-                  Finalizar Compra
-                </button>} */}
-              <button className="btnAddCart" onClick={handleBuy}>
-                Finalizar compra
-              </button>
-              {preferenceId && (
-                <Wallet
-                  initialization={{ preferenceId: preferenceId }}
-                  customization={{ texts: { valueProp: "smart_option" } }}
-                />
-              )}
+          ) : (
+            <div className="text-center">
+              <h2>No hay nada para pagar</h2>
+              <Link to={"/"}>
+                <button className="btnAddCart mb-5">Volver a Inicio</button>
+              </Link>
             </div>
-          </div>
+          )}
         </div>
       </div>
-      {/* </form> */}
     </div>
   );
 }
